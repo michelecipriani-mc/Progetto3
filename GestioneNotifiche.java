f73ed8e38118fd3c5d864b00382e19275799a126
GiovanniDadone <143901741+GiovanniDadone@users.noreply.github.com>
2025-07-09 14:37:00 +0200

    Update GestioneNotifiche.java

--- GestioneNotifiche.java
+++ GestioneNotifiche.java
@@ -83,7 +83,7 @@ public class GestioneNotifiche {
             scelta = myScannerInt.nextInt();
             // effttuo una verifica di correttezza della scelta altrimenti richiedo
 
-            while (scelta < 0 || scelta > 5) {
+            while (scelta < 0 || scelta > 4) {
                 System.out.println("Errore: comando non valido, inserisci un comando valido: ");
                 scelta = myScannerInt.nextInt();
             }
